f573c603abb56a699d66b21bde3e4003f3ddcac5
Jose Pindado <joseangelpindado@gmail.com>
2024-12-14 22:24:13 +0000

    update

--- src/models.py
+++ src/models.py
@@ -1,29 +1,84 @@
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Column, ForeignKey, Integer, String, Text
 from sqlalchemy.orm import relationship, declarative_base
 from sqlalchemy import create_engine
 from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
+class User(Base):
+    __tablename__ = 'user'
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    username = Column(String(30), unique=True, nullable=False)
+    first_name = Column(String(50), nullable=False)
+    last_name = Column(String(50), nullable=False)
+    email = Column(String(100), unique=True, nullable=False)
+    password = Column(String(100), nullable=False)
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+    posts = relationship('Post', back_populates='user')
+    favorites = relationship('Favorite', back_populates='user')
+
+class Post(Base):
+    __tablename__ = 'post'
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    user_id = Column(Integer, ForeignKey('user.id'), nullable=False)
+
+    user = relationship('User', back_populates='posts')
+    comments = relationship('Comment', back_populates='post')
+    media = relationship('Media', back_populates='post')
+
+class Planet(Base):
+    __tablename__ = 'planet'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(100), unique=True, nullable=False)
+    description = Column(Text)
+    climate = Column(String(50))
+    terrain = Column(String(50))
+    population = Column(Integer)
+
+    favorites = relationship('Favorite', back_populates='planet')
+
+class Character(Base):
+    __tablename__ = 'character'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(100), unique=True, nullable=False)
+    description = Column(Text)
+    species = Column(String(50))
+    gender = Column(String(50))
+    homeworld = Column(String(100))
+
+    favorites = relationship('Favorite', back_populates='character')
+
+class Favorite(Base):
+    __tablename__ = 'favorite'
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.id'), nullable=False)
+    planet_id = Column(Integer, ForeignKey('planet.id'), nullable=True)
+    character_id = Column(Integer, ForeignKey('character.id'), nullable=True)
+
+    user = relationship('User', back_populates='favorites')
+    planet = relationship('Planet', back_populates='favorites')
+    character = relationship('Character', back_populates='favorites')
+
+class Comment(Base):
+    __tablename__ = 'comment'
+    id = Column(Integer, primary_key=True)
+    post_id = Column(Integer, ForeignKey('post.id'), nullable=False)
+    author_id = Column(Integer, ForeignKey('user.id'), nullable=False)
+    comment_text = Column(Text, nullable=False)
+
+    post = relationship('Post', back_populates='comments')
+    author = relationship('User')
+
+class Media(Base):
+    __tablename__ = 'media'
+    id = Column(Integer, primary_key=True)
+    post_id = Column(Integer, ForeignKey('post.id'), nullable=False)
+    type = Column(String(50), nullable=False)  # Enum type could be defined if you have fixed types
+    url = Column(String(255), nullable=False)
+
+    post = relationship('Post', back_populates='media')
 
     def to_dict(self):
         return {}
